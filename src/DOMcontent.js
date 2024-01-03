aa6323f5b7fb6896f57a067c70105ad108391c90
kamron-dev <k.kamron@yadnex.ru>
2024-01-03 17:29:51 +0500

    working menu button

--- src/DOMcontent.js
+++ src/DOMcontent.js
@@ -1,56 +1,73 @@
+import createMenu from "./menu";
+
+
 export default function renderPage() {
+    document.addEventListener("DOMContentLoaded", () => {
+        //Placing the main div into a variable
+        const mainDiv = document.querySelector("#content");
+        let menuButton, aboutButton, locationButton, contentDiv;
     
-    //Placing the main div into a variable
-    const mainDiv = document.querySelector("#content");
+        function createHeader() {
+            const header = document.createElement("header");
+            header.setAttribute("id", "header");
+            const placeName = document.createElement("h1");
+            placeName.setAttribute("id", "placeName");
+            placeName.textContent = "Mama Pizza!";
+            header.appendChild(placeName);
+            mainDiv.appendChild(header);
 
-    function createHeader() {
-        const header = document.createElement("header");
-        header.setAttribute("id", "header");
-        const placeName = document.createElement("h1");
-        placeName.setAttribute("id", "placeName");
-        placeName.textContent = "Mama Pizza!";
-        header.appendChild(placeName);
-        mainDiv.appendChild(header);
+            return header;
+        };
 
-        return mainDiv;
-    };
+        function createNav() {
+            const navElement = document.createElement("nav");
+            menuButton = document.createElement("button");
+            aboutButton = document.createElement("button");
+            locationButton = document.createElement("button");
+            
+            menuButton.textContent = "Menu";
+            menuButton.setAttribute("id", "menuBtn");
+            menuButton.classList.add("nav-button");
+            navElement.appendChild(menuButton);
+            
+            aboutButton.textContent = "About Us";
+            aboutButton.setAttribute("id", "aboutBtn");
+            aboutButton.classList.add("nav-button");
+            navElement.appendChild(aboutButton);
+            
+            locationButton.textContent = "Location";
+            locationButton.setAttribute("id", "locationBtn");
+            locationButton.classList.add("nav-button");
+            navElement.appendChild(locationButton);
+            
+            header.appendChild(navElement);
+            
 
-    function createNav() {
-        const navElement = document.createElement("nav");
-        const menuButton = document.createElement("button");
-        const aboutButton = document.createElement("button");
-        const locationButton = document.createElement("button");
+            return { menuButton, aboutButton, locationButton };
+        };
+
+        function createMain() {
+            contentDiv = document.createElement("div");
+            contentDiv.setAttribute("id", "content-Div");
+            mainDiv.appendChild(contentDiv);
+
+            return contentDiv;
+        };
+
         
-        menuButton.textContent = "Menu";
-        menuButton.setAttribute("id", "menuBtn");
-        navElement.appendChild(menuButton);
+        createHeader();
+        createNav();
+        createMain();
         
-        aboutButton.textContent = "About Us";
-        aboutButton.setAttribute("id", "aboutBtn");
-        navElement.appendChild(aboutButton);
+        menuButton.addEventListener("click", () => {
+            console.log("menu clicked!");
+            contentDiv.innerHTML = '';
+            createMenu();
+        });
+
+
         
-        locationButton.textContent = "Location";
-        locationButton.setAttribute("id", "locationBtn");
-        navElement.appendChild(locationButton);
-        
-        header.appendChild(navElement);
-
-        return mainDiv;
-    };
-
-    function createMain() {
-        const contentDiv = document.createElement("div");
-        contentDiv.setAttribute("id", "content-Div");
-        mainDiv.appendChild(contentDiv);
-
-        return mainDiv
-    }
-
-
-    createHeader();
-    createNav();
-    createMain();
-
+    });
     
     
 };
