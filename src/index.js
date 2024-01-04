0f760540e3120ba63ebcf643aa4450cbd7eb6319
kamron-dev <k.kamron@yadnex.ru>
2024-01-04 16:46:29 +0500

    everything done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import renderPage from "./DOMcontent";
 
-// const menuBtn = document.querySelector("munuBtn");
-// menuBtn.addEventListener("click", () => {
-//     console.log("menu clicked!");
-// });
-
-console.log("working!")
 renderPage();
